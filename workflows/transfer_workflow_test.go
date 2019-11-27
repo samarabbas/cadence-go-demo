c55525131338708b31ee7c37d82a455992bef2bf
Maxim Fateev <mfateev@gmail.com>
2019-11-27 10:30:16 -0800

    Added TestWorkflowWithdrawFailure unit test

--- workflows/transfer_workflow_test.go
+++ workflows/transfer_workflow_test.go
@@ -1,6 +1,8 @@
 package workflows
 
 import (
+	"errors"
+	"github.com/stretchr/testify/mock"
 	"testing"
 
 	_ "github.com/samarabbas/cadence-go-demo/activities"
@@ -8,7 +10,7 @@ import (
 	"go.uber.org/cadence/testsuite"
 )
 
-func TestSuccess(t *testing.T) {
+func TestWorkflowWithRealActivities(t *testing.T) {
 	suite := testsuite.WorkflowTestSuite{}
 	env := suite.NewTestWorkflowEnvironment()
 	request := AccountTransferRequest{
@@ -19,4 +21,21 @@ func TestSuccess(t *testing.T) {
 	}
 	env.ExecuteWorkflow(transferWorkflow, request)
 	assert.NoError(t, env.GetWorkflowError())
+}
+
+func TestWorkflowWithdrawFailure(t *testing.T) {
+	suite := testsuite.WorkflowTestSuite{}
+	env := suite.NewTestWorkflowEnvironment()
+	env.OnActivity("withdraw", mock.Anything, "account1", "reference1", 1000).
+		Return(errors.New("simulated failure")).
+		Times(100)
+	request := AccountTransferRequest{
+		FromAccountId: "account1",
+		ToAccountId:   "account2",
+		ReferenceId:   "reference1",
+		Amount:        1000,
+	}
+	env.ExecuteWorkflow(transferWorkflow, request)
+	assert.Error(t, env.GetWorkflowError())
+	assert.Equal(t, "simulated failure", env.GetWorkflowError().Error())
 }
